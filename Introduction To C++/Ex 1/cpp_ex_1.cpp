05a5dfecd03cefe60186d743eddfc871b62cc9c9
Oleg Lutsik <114278831+CleanRound@users.noreply.github.com>
2022-12-27 18:32:52 +0200

    Update: upload cpp_ex_1.cpp, console_app_1 files

---
+++ Introduction To C++/Ex 1/cpp_ex_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Name\t\t: " << "\"The war and the peace\"" << endl;
+	cout << "Avtor\t\t: " << "L.N. Tolstoj" << endl;
+	cout << "Izdatelstvo\t: " << "Piter" << endl;
+	cout << "Pages\t\t: " << "500" << endl;
+	return 0;
+}

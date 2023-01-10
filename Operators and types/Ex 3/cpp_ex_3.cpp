8970966095966d3393edad2ebf70053eabadcbda
Oleg Lutsik <114278831+CleanRound@users.noreply.github.com>
2023-01-11 01:22:50 +0200

    Update: upload cpp_ex_3.cpp, console_app_3 files

---
+++ Operators and types/Ex 3/cpp_ex_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b, h;
+
+	cout << "Enter the length value: ";
+	cin >> a;
+	cout << "Enter the width value: " ;
+	cin >> b;
+	cout << "Enter the height value: " ;
+	cin >> h;
+	cout << "" << endl;
+	float CalculateVolume = a * b * h;
+
+	cout << "The volume of a parallelepiped is " << CalculateVolume << " cubic centimetres";
+	return 0;
+}
